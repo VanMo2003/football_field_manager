0af1c15f4cfb18d8376cd05a4512992dafd06714
VanMo3923 <mjnhhuyen0@gmail.com>
2025-08-27 19:09:45 +0700

    init project

---
+++ football_field_manager/src/main/java/com/example/football_field_manager/FootballFieldManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.football_field_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballFieldManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballFieldManagerApplication.class, args);
+	}
+
+}
